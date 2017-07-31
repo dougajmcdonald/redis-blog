cf51fcb1b071ca5ecbf820a43370cc5dea20daec
Doug McDonald <DougM@dsllan.net>
2017-07-31 13:46:11 +0100

    try another redis client

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 
 var app = require('express')();
 var http = require('http').Server(app);
-var Redis = require('ioredis');
+var redis = require('redis');
 
-var redis = new Redis({
-  port: 6379,
-  host: 'dougmcdonald.redis.cache.windows.net',
-  family: 4,
-  password: 'mSSiiSPJ9tszsEmX0lSCZD96Bv/yX3lTb6RC/Z35ISo='
-});
+var redisClient = new redis.createClient(
+  6379,
+  'dougmcdonald.redis.cache.windows.net',
+  {
+    auth_pass: 'mSSiiSPJ9tszsEmX0lSCZD96Bv/yX3lTb6RC/Z35ISo=',
+    tls: {
+      servername: 'dougmcdonald.redis.cache.windows.net'
+    }
+  }
+);
 
 app.get('/', function(req, res) {
-  redis.get('index', function (err, result) {
+  redisClient.get('index', function (err, result) {
     console.log('err', err);
     console.log('result', result);
     res.send(result);
