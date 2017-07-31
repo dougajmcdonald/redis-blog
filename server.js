06483b9b08930a746623c02b2484ca3eac06ae37
Doug McDonald <DougM@dsllan.net>
2017-07-31 13:06:41 +0100

    initial checknin

---
+++ server.js
@@ -0,0 +1,24 @@
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var Redis = require('ioredis');
+
+var redis = new Redis({
+  port: 6380,
+  host: dougmcdonald.redis.cache.windows.net,
+  family: 4,
+  password: 'mSSiiSPJ9tszsEmX0lSCZD96Bv/yX3lTb6RC/Z35ISo='
+});
+
+app.get('/', function(req, res) {
+
+  redis.get('index', function (err, result) {
+    //console.log(result);
+    res.send(result);
+  });
+
+  //res.sendFile(__dirname + '/index.html');
+});
+
+
